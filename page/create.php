0e75b5968426c7852270ed37957f8db37b525999
Muhamamd Mehdi <mahdikhan660@gmail.com>
2024-08-15 05:38:41 +0500

    UI fixes. JS Refactored

--- page/create.php
+++ page/create.php
@@ -8,15 +8,11 @@ include('../secure.php');
         <div id="outer-quiz-box">
             <form id="create-quiz-form" action="../db/process_create.php" method="post">
                 <div id="quiz-box">
-                    <input id="create-title" type="text" placeholder="Quiz Title" name="title" onmouseenter="myMouse()"
-                        onmouseleave="mouse()" />
-                    <!-- <textarea id="title" placeholder="Quiz Title" name="title" onmouseenter="myMouse()" onmouseleave="mouse()"></textarea>
-                 -->
-                    <textarea id="create-description" placeholder="Description" name="description" onmouseenter="des()"
-                        onmouseleave="cription()"></textarea>
-                    <input onmouseenter="cate()" onmouseleave="gory()" id="category" type="text" name="category"
+                    <input id="create-title" class="mouse-effect" type="text" placeholder="Quiz Title" name="title" />
+                    <textarea id="create-description" class="mouse-effect" placeholder="Description" name="description"></textarea>
+                    <input id="category" class="mouse-effect" type="text" name="category"
                         placeholder="Category: Cat / Dog / Bird / Fish etc" />
-                    <select onmouseenter="diff()" onmouseleave="ficulty()" id="difficulty" name="difficulty">
+                    <select id="difficulty" class="mouse-effect" name="difficulty">
                         <option value="1" selected disabled hidden>Difficulty</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -29,21 +25,19 @@ include('../secure.php');
                 <ol id="ordered-list-question">
                     <div class="question-box">
                         <li>
-                            <input id="question" onmouseenter="question()" onmouseleave="box()"
-                                class="question-statement" type="text" name="questions[0][statement]"
+                            <input id="question" 
+                                class="question-statement mouse-effect" type="text" name="questions[0][statement]"
                                 placeholder="Write your question statement here" />
-                            <!-- <textarea id="question" class="question-statement" name="questions[0][statement]" placeholder="Write your question statement here" onmouseenter="question()" onmouseleave="box()"></textarea> -->
-
                             <ol id="ordered-list-option" class="ordered-list-option">
                                 <li>
-                                    <input onmouseenter="option()" onmouseleave="boxes()" class="question-option"
-                                        type="text" name="questions[0][options][]" id="option"
+                                    <input id="option" class="question-option mouse-effect"
+                                        type="text" name="questions[0][options][]"
                                         placeholder="Write your option here" />
                                     <span id="add-option-icon-box"><i
                                             class='add-option-icon bx bx-plus-circle'></i></span>
                                 </li>
                             </ol>
-                            <input class="correct-option" onmouseenter="number()" onmouseleave="select()" type="number"
+                            <input class="correct-option mouse-effect" type="number"
                                 name="questions[0][correct_option]" placeholder="Correct option number">
                         </li>
                     </div>
